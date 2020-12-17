c8ca6dcd9616c28dedab41dc95f267ab968f576c
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-12-17 09:18:32 -0300

    started working on routes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-import express from 'express'
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => res.send('Hello World'));
+app.listen(PORT, () => {
+    console.log(`Server running o port: http://localhost:${PORT}`)
+})
